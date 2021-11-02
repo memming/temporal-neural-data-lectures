52e8e558ace29bb0dfede07624231a0b1617f17f
Memming Park <>
2021-11-01 20:59:13 -0400

    compressing a bit

--- code/worksheet_simGLM.m
+++ code/worksheet_simGLM.m
@@ -3,7 +3,7 @@ T = 1000; % just 1000 time points, try increasing this if you'd like
 x = randn(T, 1); % white noise stimulus
 y = zeros(T, 1); % pre-allocate some space to put the spikes
 for t = 4:T % start from time 4, because we need y(t-3)
-    y(t) = poissrnd(exp(-5 * y(t-1) + -0.5 * y(t-2) + -0.1 * y(t-3) + 0.3 * x(t) - 3));
+    y(t) = poissrnd(exp(-5*y(t-1) - 1.5*y(t-2) - 0.1*y(t-3) + 0.3*x(t)-3));
 end
 %% form a design matrix for glmfit
 Y = y(4:T);
