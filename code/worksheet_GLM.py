2c5dc57e89591d952c97393eca7c525cb0c67953
Asagodi <abel.sagodi@gmail.com>
2023-01-18 08:52:22 +0000

    glm comments

--- code/worksheet_GLM.py
+++ code/worksheet_GLM.py
@@ -1,5 +1,10 @@
 import statsmodels.api as sm
-log_link = sm.families.links.Log()
-poisson_log = sm.GLM(Y, X, family=sm.families.Poisson(log_link))
-poisson_log_results = poisson_log.fit()
-poisson_log_results.summary()
+
+log_link = sm.families.links.Log() #defines as the link function the logarithm
+
+#initialize Poisson regression: 
+poisson_log = sm.GLM(Y, X, family=sm.families.Poisson(log_link)) 
+
+poisson_log_results = poisson_log.fit() #fit model 
+
+poisson_log_results.summary() #get summary of fit
